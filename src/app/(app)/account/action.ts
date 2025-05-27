843a8af14700f8c707588fb38aa5a85b0fc510ba
Mike Codeur <contact@mikecodeur.com>
2025-05-27 21:27:39 +0800

    flip

--- src/app/(app)/account/action.ts
+++ src/app/(app)/account/action.ts
@@ -3,13 +3,19 @@
 import {revalidatePath} from 'next/cache'
 import {UpdateUser} from '@/services/types/domain/user-types'
 import {updateUserService} from '@/services/user-service'
+import {getAuthUser} from '@/services/authentication/auth-utils'
+
 export async function updateUser(userId: string, formData: FormData) {
+  const user = await getAuthUser()
+  if (!user) {
+    return {success: false, message: 'User not found'}
+  }
   const name = formData.get('name') as string
   const email = formData.get('email') as string
   const image = formData.get('image') as string
   const visibility = formData.get('visibility') as 'public' | 'private'
 
-  const user: UpdateUser = {
+  const userData: UpdateUser = {
     id: userId,
     name,
     email,
@@ -17,7 +23,7 @@ export async function updateUser(userId: string, formData: FormData) {
     visibility,
   }
   try {
-    await updateUserService(userId, user)
+    await updateUserService(userId, userData)
     revalidatePath('/account')
     return {success: true, message: 'Profile updated successfully'}
   } catch (error) {
