c360c690a96a3d510379e2459a778eeaca6c667b
Mike Codeur <contact@mikecodeur.com>
2025-06-04 14:54:47 +0800

    approvider

--- src/app/base-layout.tsx
+++ src/app/base-layout.tsx
@@ -1,9 +1,7 @@
 import {GoogleAnalytics} from '@next/third-parties/google'
 import React, {ReactNode} from 'react'
-import {Toaster} from 'sonner'
 
-import NextAuthProvider from '@/components/context/auth-provider'
-import {ThemeProvider} from '@/components/context/theme-provider'
+import {AppProviders} from '@/components/context/app-providers'
 
 type Props = {
   children: ReactNode
@@ -14,16 +12,7 @@ export default async function BaseLayout({children}: Props) {
   return (
     <html lang={'fr'} suppressHydrationWarning>
       <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          storageKey="theme"
-          disableTransitionOnChange
-        >
-          <NextAuthProvider>{children}</NextAuthProvider>
-          <Toaster />
-        </ThemeProvider>
+        <AppProviders>{children}</AppProviders>
       </body>
       <GoogleAnalytics gaId={gtag} />
     </html>
---
+++ src/components/context/app-providers.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import {PropsWithChildren} from 'react'
+import {Toaster} from 'sonner'
+
+import {User} from '@/services/types/domain/user-types'
+
+import NextAuthProvider from './auth-provider'
+import {ThemeProvider} from './theme-provider'
+
+interface AppProvidersProps extends PropsWithChildren {
+  initialUser?: User | null
+}
+
+export function AppProviders({children}: AppProvidersProps) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      storageKey="theme"
+      disableTransitionOnChange
+    >
+      <NextAuthProvider>
+        {children}
+        <Toaster />
+      </NextAuthProvider>
+    </ThemeProvider>
+  )
+}
