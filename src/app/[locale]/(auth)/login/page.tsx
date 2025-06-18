654ee417ae841731672c22d5cf8fa09086f2ae53
Mike Codeur <contact@mikecodeur.com>
2025-06-18 10:38:37 +0800

    login page

--- src/app/[locale]/(auth)/login/page.tsx
+++ src/app/[locale]/(auth)/login/page.tsx
@@ -1,9 +1,15 @@
 import {GalleryVerticalEnd} from 'lucide-react'
+import {redirect} from 'next/navigation'
 
 import {LoginForm} from '@/components/features/auth/forms/login'
 import {APP_NAME} from '@/lib/constants'
+import {getAuthUser} from '@/services/authentication/auth-service'
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const user = await getAuthUser()
+  if (user) {
+    redirect('/logout')
+  }
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
