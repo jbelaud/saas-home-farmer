48384ed5a0afdac0b3f676484be049e93ea6a147
Mike Codeur <contact@mikecodeur.com>
2025-07-07 21:36:15 +0800

    Refactorisation de la page de checkout avec une nouvelle gestion des plans d'abonnement. Remplacement des appels à `getSubscriptionRecapInfo` par des fonctions du DAL pour récupérer les plans disponibles. Ajout de vérifications pour s'assurer que tous les plans sont chargés correctement. Amélioration de la clarté et de la maintenabilité du code.

--- src/app/[locale]/(public)/checkout/better-auth/page.tsx
+++ src/app/[locale]/(public)/checkout/better-auth/page.tsx
@@ -1,50 +1,48 @@
-import {getSubscriptionRecapInfo} from '@/components/features/checkout-stripe/actions'
-import {env} from '@/env'
+import {
+  getEntreprisePlan,
+  getFreePlan,
+  getLifetimePlan,
+  getProPlan,
+} from '@/app/dal/subscription-dal'
+import {getSubscriptionRecap} from '@/lib/stripe/stripe-utils'
 
 import CheckoutBetterAuth from './checkout-better-auth'
 
 export default async function Page() {
   // permet un gestion dynamique des prix (si modifié coté dashboard stripe)
-  const [
-    proMonthlyResult,
-    proYearlyResult,
-    lifetimeResult,
-    entrepriseMonthlyResult,
-    entrepriseYearlyResult,
-  ] = await Promise.all([
-    getSubscriptionRecapInfo(
-      env.NEXT_PUBLIC_STRIPE_PRICE_ID_PRO_MONTHLY,
-      undefined,
-      1
-    ),
-    getSubscriptionRecapInfo(
-      env.NEXT_PUBLIC_STRIPE_PRICE_ID_PRO_YEARLY,
-      undefined,
-      1
-    ),
-    getSubscriptionRecapInfo(
-      env.NEXT_PUBLIC_STRIPE_PRICE_ID_LIFETIME,
-      undefined,
-      1
-    ),
-    getSubscriptionRecapInfo(
-      env.NEXT_PUBLIC_STRIPE_PRICE_ID_ENTREPRISE_MONTHLY,
-      undefined,
-      1
-    ),
-    getSubscriptionRecapInfo(
-      env.NEXT_PUBLIC_STRIPE_PRICE_ID_ENTREPRISE_YEARLY,
-      undefined,
-      1
-    ),
+  const [planFree, planPro, planEntreprise, planLifetime] = await Promise.all([
+    getFreePlan(),
+    getProPlan(),
+    getEntreprisePlan(),
+    getLifetimePlan(),
   ])
 
+  // Vérification que tous les plans sont disponibles
+  if (!planFree || !planPro || !planEntreprise) {
+    throw new Error('Impossible de charger les plans')
+  }
+
+  const priceProMonthly = await getSubscriptionRecap(planPro?.priceId)
+  const priceProYearly = await getSubscriptionRecap(
+    planPro?.annualDiscountPriceId as string
+  )
+
+  const priceEntrepriseMonthly = await getSubscriptionRecap(
+    planEntreprise?.priceId
+  )
+  const priceEntrepriseYearly = await getSubscriptionRecap(
+    planEntreprise?.annualDiscountPriceId as string
+  )
+  const priceLifetime = await getSubscriptionRecap(
+    planLifetime?.priceId as string
+  )
+
   const initialPriceRecaps = {
-    proMonthly: proMonthlyResult.recap,
-    proYearly: proYearlyResult.recap,
-    lifetime: lifetimeResult.recap,
-    entrepriseMonthly: entrepriseMonthlyResult.recap,
-    entrepriseYearly: entrepriseYearlyResult.recap,
+    proMonthly: priceProMonthly.recap,
+    proYearly: priceProYearly.recap,
+    lifetime: priceLifetime.recap,
+    entrepriseMonthly: priceEntrepriseMonthly.recap,
+    entrepriseYearly: priceEntrepriseYearly.recap,
   }
 
   return <CheckoutBetterAuth initialPriceRecaps={initialPriceRecaps} />
