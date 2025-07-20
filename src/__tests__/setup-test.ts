c17f921b816e23a8a8ab3b74c60019a7a2dd93be
Mike Codeur <contact@mikecodeur.com>
2025-07-20 13:05:55 +0800

    Mise à jour de la version de '@testing-library/react' de 16.3.0 à 15.0.7 dans package.json et pnpm-lock.yaml pour assurer la compatibilité avec les autres dépendances.

--- src/__tests__/setup-test.ts
+++ src/__tests__/setup-test.ts
@@ -17,27 +17,3 @@ beforeAll(() => {
     }),
   })
 })
-
-import * as React from 'react'
-import * as TestUtils from 'react-dom/test-utils'
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-if (typeof (React as any).act !== 'function') {
-  // Vérifie que la propriété n’est pas "read-only"
-  const descriptor = Object.getOwnPropertyDescriptor(React, 'act')
-
-  if (!descriptor || descriptor.writable || descriptor.configurable) {
-    console.warn(
-      '⚠️ React.act is missing, patching with react-dom/test-utils.act'
-    )
-
-    // eslint-disable-next-line no-import-assign
-    Object.defineProperty(React, 'act', {
-      value: TestUtils.act,
-      writable: true,
-      configurable: true,
-    })
-  } else {
-    console.warn('❌ React.act is missing but not patchable (read-only)')
-  }
-}
