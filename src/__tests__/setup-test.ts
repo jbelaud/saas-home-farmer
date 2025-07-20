e065fa299efc9c98b7f3cf9f34a9686113cc495c
Mike Codeur <contact@mikecodeur.com>
2025-07-20 12:42:35 +0800

    Mise à jour de la version de pnpm dans package.json et ajout d'une vérification pour la méthode 'act' dans les tests afin d'assurer la compatibilité avec React.

--- src/__tests__/setup-test.ts
+++ src/__tests__/setup-test.ts
@@ -1,6 +1,16 @@
 import '@testing-library/jest-dom/vitest'
 
+import * as React from 'react'
+import * as TestUtils from 'react-dom/test-utils'
 import {beforeAll, vi} from 'vitest'
+
+// Problems github action, will be removed
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+if (typeof (React as any).act !== 'function') {
+  console.warn('⚠️ React.act is missing, falling back to test-utils.act')
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ;(React as any).act = TestUtils.act
+}
 
 beforeAll(() => {
   Object.defineProperty(window, 'matchMedia', {
