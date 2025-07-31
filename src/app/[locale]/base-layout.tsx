0b30ca948f342d7d7f0afac6a516383fbac129d5
Mike Codeur <contact@mikecodeur.com>
2025-07-31 15:41:29 +0800

    fix: supprimer la fonction generateMetadata et ajuster l'importation de getTranslations pour simplifier la gestion des traductions dans le layout de base

--- src/app/[locale]/base-layout.tsx
+++ src/app/[locale]/base-layout.tsx
@@ -1,6 +1,6 @@
 import {GoogleAnalytics} from '@next/third-parties/google'
 import {NextIntlClientProvider} from 'next-intl'
-import {getTranslations, setRequestLocale} from 'next-intl/server'
+import {setRequestLocale} from 'next-intl/server'
 import React, {ReactNode} from 'react'
 
 import {AppProviders} from '@/components/context/app-providers'
@@ -29,26 +29,6 @@ export default async function BaseLayout({children, locale}: Props) {
     </html>
   )
 }
-
-// Déplacer generateMetadata après le composant principal
-export async function generateMetadata({
-  params,
-}: {
-  params: Promise<{locale: string}>
-}) {
-  const paramsStore = await params
-  const locale = paramsStore.locale
-
-  // Configurer la locale avant getTranslations
-  setRequestLocale(locale)
-  const t = await getTranslations({locale, namespace: 'LocaleLayout'})
-
-  return {
-    title: t('title'),
-    description: t('description'),
-  }
-}
-
 export function generateStaticParams() {
   return routing.locales.map((locale) => ({locale}))
 }
