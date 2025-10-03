3a3d5712612f308a4b7c32355e617f42738a8666
Mike Codeur <contact@mikecodeur.com>
2025-10-03 22:24:34 +0800

    feat: ajouter une règle de sécurisation des routes et layouts avec HOCs `withAuth` et `withAuthAdmin`, renforçant ainsi la sécurité des applications Next.js 15 en suivant une architecture multi-couches

--- src/app/[locale]/admin/organizations/[id]/page.tsx
+++ src/app/[locale]/admin/organizations/[id]/page.tsx
@@ -1,11 +1,11 @@
 import {redirect} from 'next/navigation'
 
-export default async function EditOrganizationPage({
-  params,
-}: {
-  params: Promise<{id: string}>
-}) {
+import {withAuthAdmin} from '@/components/features/auth/with-auth'
+
+async function EditOrganizationPage({params}: {params: Promise<{id: string}>}) {
   const {id} = await params
   redirect(`/admin/organizations/${id}/edit`)
   return <></>
 }
+
+export default withAuthAdmin(EditOrganizationPage)
